aaaa03a5f9256e8fe09884abea9e51fc9654c0fd
ltrii <lelandtr@gmail.com>
2019-02-26 12:28:58 -0800

    Session implemented

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const db = require('./data/dbConfig.js');
+const Users = require('./models/usersModel.js');
 
 
 const server = express();
 
+
+const sessionConfig = {
+  name: 'denizen',
+  secret: 'a big secret',
+  cookie: {
+    maxAge: 24 * 60 * 60, // 1 day
+    secure: false, // for HTTPS
+  },
+  httpOnly: true, 
+  resave: false,
+  saveUninitialized: false, // GDPR 
+
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 24 * 60 * 60, // in ms
+  }),
+};
+
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 //ROUTES
 
@@ -50,23 +78,10 @@ server.get('/', (req, res) => {
   });
   
   function restricted(req, res, next) {
-    let { username, password } = req.headers;
-  
-    if(username && password){
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Unexpected error'});
-      });
+    if (req.session && req.session.user) {
+      next();
     } else {
-      res.status(400).json({ message: 'No credentials provided'});
+      res.status(401).json({ message: 'You shall not pass!' });
     }
   }
   
